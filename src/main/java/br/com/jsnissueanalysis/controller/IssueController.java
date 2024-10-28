64542515543313251b38cf58995ea4c752baa9f6
sleevs <jsnprogramer@gmail.com>
2024-10-28 16:41:52 -0300

    feature : implementação de descrição na  documentação da api

--- src/main/java/br/com/jsnissueanalysis/controller/IssueController.java
+++ src/main/java/br/com/jsnissueanalysis/controller/IssueController.java
@@ -11,6 +11,9 @@ import br.com.jsnissueanalysis.dto.ResponseDto;
 import br.com.jsnissueanalysis.dto.UserDto;
 import br.com.jsnissueanalysis.service.GitHubService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import reactor.core.publisher.Mono;
 
 
@@ -23,10 +26,18 @@ public class IssueController {
         this.gitHubService = gitHubService;
     }
 
-
-    @PostMapping(value = "/v1/request-after")
-@CircuitBreaker(name = "requestIssues", fallbackMethod = "fallbackMethod")
-public Mono<ResponseDto> requestIssuesAfter(@RequestBody RequestDto request) {
+@Operation(
+summary = "Enviar issues Depois",
+description = "Realiza um request para GITHUB API e retorna um JSON assincronamente com 1 dia de diferença e via webhook com as issues e contribuidores que existiam no projeto no momento da chamada.",
+tags = {"Enviar issues"}
+)
+@ApiResponses(value = {
+@ApiResponse(responseCode = "200", description = "Issues enviada com sucesso"),
+@ApiResponse(responseCode = "400", description = "Requisição inválida")
+})
+@PostMapping(value = "/v1/send-after")
+@CircuitBreaker(name = "sendAfter", fallbackMethod = "fallbackMethod")
+public Mono<ResponseDto> sendAfter(@RequestBody RequestDto request) {
     
     if (request.getUsername() == null || request.getUsername().isEmpty()) {
         return Mono.error(new IllegalArgumentException("O nome do usuário não pode ser nulo ou vazio."));
@@ -49,10 +60,18 @@ public Mono<ResponseDto> requestIssuesAfter(@RequestBody RequestDto request) {
     }
 
 
-
- @PostMapping(value = "/v1/request-now")
- @CircuitBreaker(name = "requestIssues", fallbackMethod = "fallbackMethod")
- public Mono<ResponseDto> requestIssuesNow(@RequestBody RequestDto request) {
+@Operation(
+summary = "Receber issues agora",
+description = "Realiza um request para GITHUB API e retorna um JSON com as issues e contribuidores que existiam no projeto no momento da chamada.",
+tags = {"Receber agora"}
+)
+@ApiResponses(value = {
+@ApiResponse(responseCode = "200", description = "Issues recuperada com sucesso"),
+@ApiResponse(responseCode = "400", description = "Requisição inválida")
+})   
+ @PostMapping(value = "/v1/send-now")
+ @CircuitBreaker(name = "sendNow", fallbackMethod = "fallbackMethod")
+ public Mono<ResponseDto> sendNow(@RequestBody RequestDto request) {
      
      if (request.getUsername() == null || request.getUsername().isEmpty()) {
          return Mono.error(new IllegalArgumentException("O nome do usuário não pode ser nulo ou vazio."));
